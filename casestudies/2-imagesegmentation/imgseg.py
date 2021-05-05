0c1e11ab2bbae1322be4193fb09e7562274099ea
Niranjhana Narayanan <nniranjhana@ece.ubc.ca>
2021-05-04 23:18:38 -0700

    Add case study 2 data
    Signed-off-by: Niranjhana Narayanan <nniranjhana@ece.ubc.ca>

---
+++ casestudies/2-imagesegmentation/imgseg.py
@@ -0,0 +1,166 @@
+import tensorflow as tf
+from tensorflow_examples.models.pix2pix import pix2pix
+import numpy as np
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Model
+import tensorflow.keras.backend as K
+import random, math
+from struct import pack, unpack
+
+import os
+os.environ['TF_MIN_CPP_LOG_LEVEL'] = '2'
+
+from src import tfi
+
+dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
+
+def normalize(input_image, input_mask):
+  input_image = tf.cast(input_image, tf.float32) / 255.0
+  input_mask -= 1
+  return input_image, input_mask
+
+@tf.function
+def load_image_train(datapoint):
+  input_image = tf.image.resize(datapoint['image'], (128, 128))
+  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+  if np.random.uniform() > 0.5:
+    input_image = tf.image.flip_left_right(input_image)
+    input_mask = tf.image.flip_left_right(input_mask)
+
+  input_image, input_mask = normalize(input_image, input_mask)
+
+  return input_image, input_mask
+
+def load_image_test(datapoint):
+  input_image = tf.image.resize(datapoint['image'], (128, 128))
+  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+  input_image, input_mask = normalize(input_image, input_mask)
+
+  return input_image, input_mask
+
+TRAIN_LENGTH = info.splits['train'].num_examples
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
+
+train = dataset['train'].map(load_image_train)
+test = dataset['test'].map(load_image_test)
+
+
+train_dataset = train.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+train_dataset = train_dataset.prefetch(buffer_size=BATCH_SIZE)
+test_dataset = test.batch(BATCH_SIZE)
+
+def display(display_list):
+  plt.figure(figsize=(15, 15))
+
+  title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+  for i in range(len(display_list)):
+    plt.subplot(1, len(display_list), i+1)
+    plt.title(title[i])
+    plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+    plt.axis('off')
+  plt.show()
+'''
+for image, mask in train.take(1):
+  sample_image, sample_mask = image, mask
+display([sample_image, sample_mask])
+'''
+
+OUTPUT_CHANNELS = 3
+
+base_model = tf.keras.applications.MobileNetV2(input_shape=[128, 128, 3], include_top=False)
+
+# Use the activations of these layers
+layer_names = [
+    'block_1_expand_relu',   # 64x64
+    'block_3_expand_relu',   # 32x32
+    'block_6_expand_relu',   # 16x16
+    'block_13_expand_relu',  # 8x8
+    'block_16_project',      # 4x4
+]
+layers = [base_model.get_layer(name).output for name in layer_names]
+
+# Create the feature extraction model
+down_stack = tf.keras.Model(inputs=base_model.input, outputs=layers)
+
+down_stack.trainable = False
+
+up_stack = [
+    pix2pix.upsample(512, 3),  # 4x4 -> 8x8
+    pix2pix.upsample(256, 3),  # 8x8 -> 16x16
+    pix2pix.upsample(128, 3),  # 16x16 -> 32x32
+    pix2pix.upsample(64, 3),   # 32x32 -> 64x64
+]
+
+def unet_model(output_channels):
+  inputs = tf.keras.layers.Input(shape=[128, 128, 3])
+  x = inputs
+
+  # Downsampling through the model
+  skips = down_stack(x)
+  x = skips[-1]
+  skips = reversed(skips[:-1])
+
+  # Upsampling and establishing the skip connections
+  for up, skip in zip(up_stack, skips):
+    x = up(x)
+    concat = tf.keras.layers.Concatenate()
+    x = concat([x, skip])
+
+  # This is the last layer of the model
+  last = tf.keras.layers.Conv2DTranspose(
+      output_channels, 3, strides=2,
+      padding='same')  #64x64 -> 128x128
+
+  x = last(x)
+  model = Model(inputs=inputs, outputs=x)
+  return model
+
+model = unet_model(OUTPUT_CHANNELS)
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# tf.keras.utils.plot_model(model, show_shapes=True)
+
+def create_mask(pred_mask):
+  pred_mask = tf.argmax(pred_mask, axis=-1)
+  pred_mask = pred_mask[..., tf.newaxis]
+  return pred_mask[0]
+
+def show_predictions(dataset=None, num=1):
+  if dataset:
+    for image, mask in dataset.take(num):
+      pred_mask = model.predict(image)
+      display([image[0], mask[0], create_mask(pred_mask)])
+  else:
+    display([sample_image, sample_mask,
+             create_mask(model.predict(sample_image[tf.newaxis, ...]))])
+
+# show_predictions()
+
+# EPOCHS = 5
+# VAL_SUBSPLITS = 5
+# VALIDATION_STEPS = info.splits['test'].num_examples//BATCH_SIZE//VAL_SUBSPLITS
+
+# model_history = model.fit(train_dataset, epochs=EPOCHS,
+                          # steps_per_epoch=STEPS_PER_EPOCH,
+                          # validation_steps=VALIDATION_STEPS,
+                          # validation_data=test_dataset)
+
+# model.save_weights('h5/seg.h5')
+
+model.load_weights('h5/seg.h5')
+
+tfi.inject(model=model, confFile="confFiles/sample.yaml")
+
+show_predictions(test_dataset, 1)
+test_ = test_dataset.take(1)
+for elem, label in test_.as_numpy_iterator():
+	test_loss, test_acc = model.evaluate(elem, label)
+	print(test_acc)
